12d033b5f08d4bf3af508ea135a760eca93aad93
Hassanmoriani <64978920+Hassanmoriani@users.noreply.github.com>
2020-07-11 08:41:59 +0500

    Add files via upload
    upload 3

--- index.js
+++ index.js
@@ -1,83 +1,80 @@
-class Stopwatch {
-    constructor(display, results) {
-        this.running = false;
-        this.display = display;
-        this.results = results;
-        this.laps = [];
-        this.reset();
-        this.print(this.times);
-    }
-    
-    reset() {
-        this.times = [ 0, 0, 0 ];
-    }
-    
-    start() {
-        if (!this.time) this.time = performance.now();
-        if (!this.running) {
-            this.running = true;
-            requestAnimationFrame(this.step.bind(this));
-        }
-    }
-    
-    stop() {
-        this.running = false;
-        this.time = null;
-    }
-
-    restart() {
-        if (!this.time) this.time = performance.now();
-        if (!this.running) {
-            this.running = true;
-            requestAnimationFrame(this.step.bind(this));
-        }
-        this.reset();
-    }
-    
-    step(timestamp) {
-        if (!this.running) return;
-        this.calculate(timestamp);
-        this.time = timestamp;
-        this.print();
-        requestAnimationFrame(this.step.bind(this));
-    }
-    
-    calculate(timestamp) {
-        var diff = timestamp - this.time;
-        // Hundredths of a second are 100 ms
-        this.times[2] += diff / 10;
-        // Seconds are 100 hundredths of a second
-        if (this.times[2] >= 100) {
-            this.times[1] += 1;
-            this.times[2] -= 100;
-        }
-        // Minutes are 60 seconds
-        if (this.times[1] >= 60) {
-            this.times[0] += 1;
-            this.times[1] -= 60;
-        }
-    }
-    
-    print() {
-        this.display.innerText = this.format(this.times);
-    }
-    
-    format(times) {
-        return `\
-${pad0(times[0], 2)}:\
-${pad0(times[1], 2)}:\
-${pad0(Math.floor(times[2]), 2)}`;
-    }
+var startTimerButton = document.querySelector('.startTimer');
+var pauseTimerButton = document.querySelector('.pauseTimer');
+var timerDisplay = document.querySelector('.timer');
+var startTime;
+var updatedTime;
+var difference;
+var tInterval;
+var savedTime;
+var paused = 0;
+var running = 0;
+function startTimer(){
+  if(!running){
+    startTime = new Date().getTime();
+    tInterval = setInterval(getShowTime, 1);
+// change 1 to 1000 above to run script every second instead of every millisecond. one other change will be needed in the getShowTime() function below for this to work. see comment there.   
+ 
+    paused = 0;
+    running = 1;
+timerDisplay.style.background = "#6b6a6a";
+    timerDisplay.style.cursor = "auto";
+    timerDisplay.style.color = "yellow";
+    startTimerButton.classList.add('lighter');
+    pauseTimerButton.classList.remove('lighter');
+    startTimerButton.style.cursor = "auto";
+    pauseTimerButton.style.cursor = "pointer";
+  }
 }
-
-function pad0(value, count) {
-    var result = value.toString();
-    for (; result.length < count; --count)
-        result = '0' + result;
-    return result;
+function pauseTimer(){
+  if (!difference){
+    // if timer never started, don't allow pause button to do anything
+  } else if (!paused) {
+    clearInterval(tInterval);
+    savedTime = difference;
+    paused = 1;
+    running = 0;
+    timerDisplay.style.background = "#6b6a6a";
+    timerDisplay.style.color = "#6b6a6a";
+    timerDisplay.style.cursor = "pointer";
+    startTimerButton.classList.remove('lighter');
+    pauseTimerButton.classList.add('lighter');
+    startTimerButton.style.cursor = "pointer";
+    pauseTimerButton.style.cursor = "auto";
+  } else {
+// if the timer was already paused, when they click pause again, start the timer again
+startTimer();
+  }
 }
-
-
-let stopwatch = new Stopwatch(
-    document.querySelector('.stopwatch'),
-    document.querySelector('.results'));
+function resetTimer(){
+  clearInterval(tInterval);
+  savedTime = 0;
+  difference = 0;
+  paused = 0;
+  running = 0;
+  timerDisplay.innerHTML = 'Start Timer!';
+  timerDisplay.style.background = "#6b6a6a";
+  timerDisplay.style.color = "#fff";
+  timerDisplay.style.cursor = "pointer";
+  startTimerButton.classList.remove('lighter');
+  pauseTimerButton.classList.remove('lighter');
+  startTimerButton.style.cursor = "pointer";
+  pauseTimerButton.style.cursor = "auto";
+}
+function getShowTime(){
+  updatedTime = new Date().getTime();
+  if (savedTime){
+    difference = (updatedTime - startTime) + savedTime;
+  } else {
+    difference =  updatedTime - startTime;
+  }
+  // var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  var milliseconds = Math.floor((difference % (1000 * 60)) / 100);
+hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
+  timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
+}
